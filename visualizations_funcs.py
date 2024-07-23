fd34ccfe8475fc7581be107452b2a6bb2f61974b
patiosga <giorgospatios@gmail.com>
2024-07-23 17:25:44 +0300

    Dynamic ploting space
    ploting is not fixed anymore and adjusts to the specific tree without extra function arguments

--- visualizations_funcs.py
+++ visualizations_funcs.py
@@ -22,8 +22,13 @@ def get_rectangle_width(mbr: BoundingArea):
 
 
 def visualize_tree(tree: RTree):
-    # Generate random points inside each square
-    
+    # Initialize ploting space
+    if tree.root.is_leaf:
+        ploting_space = [[min([record.location[0] for record in tree.root.elements]) - 0.1, max([record.location[0] for record in tree.root.elements]) + 0.1], 
+                         [min([record.location[1] for record in tree.root.elements]) - 0.1, max([record.location[1] for record in tree.root.elements]) + 0.1]]
+    else:
+        ploting_space = [[min([mbr.bounds[0].lower for mbr in tree.root.elements]) - 0.1, max([mbr.bounds[0].upper for mbr in tree.root.elements]) + 0.1], 
+                         [min([mbr.bounds[1].lower for mbr in tree.root.elements]) - 0.1, max([mbr.bounds[1].upper for mbr in tree.root.elements]) + 0.1]]
 
     # Plot rectangles using a recursive function that traverses the tree
     fig, ax = plt.subplots()
@@ -32,13 +37,16 @@ def visualize_tree(tree: RTree):
     def recursion(current_node: Block):
         if current_node.is_leaf:
             # Plot records of leaf
-            points = [list(record.location) for record in current_node.elements]
-            ax.scatter(*zip(*points), color='red')
+            for record in current_node.elements:
+                ax.scatter(*record.location, color='red')
+            # points = [list(record.location) for record in current_node.elements]
+            # ax.scatter(*zip(*points), color='red')
             return
         
-        for mbr in current_node.elements:
+        for i, mbr in enumerate(current_node.elements):
             corner = get_down_left_corner(mbr)
             ax.add_patch(plt.Rectangle(corner, get_rectangle_width(mbr), get_rectangle_height(mbr), fill=False, edgecolor='black'))
+            ax.text(corner[0] + get_rectangle_width(mbr)/2, corner[1] + get_rectangle_height(mbr)/2, f'R{current_node.get_level()}{i}', fontsize=8, ha='center', va='center')
         for mbr in current_node.elements:
             recursion(mbr.next_block)
 
@@ -46,12 +54,9 @@ def visualize_tree(tree: RTree):
     # Initialize the recursion with the root node
     recursion(tree.root)
 
-    # Plot records / points
-    # ax.scatter(*zip(*points), color='red')
-
     # Set the plot limits and labels
-    ax.set_xlim(0, 2)
-    ax.set_ylim(0, 2)
+    ax.set_xlim(ploting_space[0][0], ploting_space[0][1])
+    ax.set_ylim(ploting_space[1][0], ploting_space[1][1])
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_title('R-tree visualization')
@@ -64,14 +69,20 @@ if __name__ == '__main__':
     tree = RTree()
     # Create 10 random records
     records = []
-    for _ in range(26):
+    for _ in range(50):
         id = np.random.randint(0, 100)
         location = np.random.rand(2)
         record = Record(id, location, -1)
         records.append(record)
 
+
+    ploting_space = [[min([record.location[0] for record in records]), max([record.location[0] for record in records])], 
+                     [min([record.location[1] for record in records]), max([record.location[1] for record in records])]]
+
+
     # Insert records into the R-tree
-    tree.bottomUp(records)
+    for record in records:
+        tree.insert(record)
 
     # Print the tree
     print(tree)
